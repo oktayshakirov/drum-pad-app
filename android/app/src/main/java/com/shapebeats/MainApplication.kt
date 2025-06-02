f6b38df8e7ec056855a6b9046e94a2541bb915fd
oktayshakirov <oktayshakirov@gmail.com>
2025-06-03 01:46:04 +0200

    renamed to Shape Beats

--- android/app/src/main/java/com/shapebeats/MainApplication.kt
+++ android/app/src/main/java/com/shapebeats/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.drumpadapp
+package com.shapebeats
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/ShapeBeatsApp/AppDelegate.swift
+++ ios/ShapeBeatsApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "DrumPadApp",
+      withModuleName: "Shape Beats",
       in: window,
       launchOptions: launchOptions
     )
