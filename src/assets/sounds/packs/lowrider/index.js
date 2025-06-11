81656ba3715b57eca335e569cb27784a62a846db
Oktay Shakirov <oktayshakirov@gmail.com>
2025-06-11 23:29:01 +0200

    improved and compressed samples

--- src/assets/sounds/packs/lowrider/index.js
+++ src/assets/sounds/packs/lowrider/index.js
@@ -20,17 +20,21 @@ export default {
   cover: require('./cover.jpg'),
   demo: require('./demo.mp3'),
   sounds: {
+    // Drums
     kick: require('./samples/kick.mp3'),
     snare: require('./samples/snare.mp3'),
     open_hat: require('./samples/open_hat.mp3'),
     clap: require('./samples/clap.mp3'),
     hit: require('./samples/hit.mp3'),
-    drums: require('./samples/drums.mp3'),
+    drums_1: require('./samples/drums_1.mp3'),
+    drums_2: require('./samples/drums_2.mp3'),
     percussion: require('./samples/percussion.mp3'),
+    // Melodies
     melody_1: require('./samples/melody_1.mp3'),
     melody_2: require('./samples/melody_2.mp3'),
+    // Chants
     chant_yeah: require('./samples/chant_yeah.mp3'),
-    chant_breath: require('./samples/chant_breath.mp3'),
+    // SFX
     sfx_gun: require('./samples/sfx_gun.mp3'),
   },
   padConfig: [
@@ -39,12 +43,12 @@ export default {
     {id: 3, sound: 'open_hat', color: PAD_COLORS.DRUMS_MAIN},
     {id: 4, sound: 'clap', color: PAD_COLORS.DRUMS_ACCENT},
     {id: 5, sound: 'hit', color: PAD_COLORS.DRUMS_ACCENT},
-    {id: 6, sound: 'drums', color: PAD_COLORS.BASS_MAIN},
-    {id: 7, sound: 'percussion', color: PAD_COLORS.BASS_ACCENT},
-    {id: 8, sound: 'melody_1', color: PAD_COLORS.MELODY_MAIN},
-    {id: 9, sound: 'melody_2', color: PAD_COLORS.MELODY_MAIN},
-    {id: 10, sound: 'chant_yeah', color: PAD_COLORS.MELODY_ACCENT},
-    {id: 11, sound: 'chant_breath', color: PAD_COLORS.MELODY_ACCENT},
+    {id: 6, sound: 'drums_1', color: PAD_COLORS.BASS_MAIN},
+    {id: 7, sound: 'drums_2', color: PAD_COLORS.BASS_MAIN},
+    {id: 8, sound: 'percussion', color: PAD_COLORS.BASS_ACCENT},
+    {id: 9, sound: 'melody_1', color: PAD_COLORS.MELODY_MAIN},
+    {id: 10, sound: 'melody_2', color: PAD_COLORS.MELODY_MAIN},
+    {id: 11, sound: 'chant_yeah', color: PAD_COLORS.MELODY_ACCENT},
     {id: 12, sound: 'sfx_gun', color: PAD_COLORS.SFX_MAIN},
   ],
 };
