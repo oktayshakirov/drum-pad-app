5b66a7eba0386a23d5bb4544f73bea12626f60da
Oktay Shakirov <oktayshakirov@gmail.com>
2025-07-24 12:38:49 +0200

    more sounds icons

--- src/assets/sounds/icons/index.ts
+++ src/assets/sounds/icons/index.ts
@@ -14,6 +14,17 @@ import HitIcon from './hit.svg';
 import WalkieTalkieIcon from './walkieTalkie.svg';
 import SwordIcon from './sword.svg';
 import ReverseIcon from './reverse.svg';
+import BassIcon from './bass.svg';
+import PercussionIcon from './percussion.svg';
+import TrumpetIcon from './trumpet.svg';
+import GunIcon from './gun.svg';
+import SFXIcon from './sfx.svg';
+import TriangleIcon from './triangle.svg';
+import ShakerIcon from './shaker.svg';
+import FluteIcon from './flute.svg';
+import SnapIcon from './snap.svg';
+import BellsIcon from './bells.svg';
+import ChoirMaleIcon from './choir_male.svg';
 
 export const iconMap: Record<string, React.FC<any>> = {
   drums: DrumsIcon,
@@ -32,4 +43,15 @@ export const iconMap: Record<string, React.FC<any>> = {
   walkieTalkie: WalkieTalkieIcon,
   sword: SwordIcon,
   reverse: ReverseIcon,
+  bass: BassIcon,
+  percussion: PercussionIcon,
+  trumpet: TrumpetIcon,
+  gun: GunIcon,
+  sfx: SFXIcon,
+  triangle: TriangleIcon,
+  shaker: ShakerIcon,
+  flute: FluteIcon,
+  snap: SnapIcon,
+  bells: BellsIcon,
+  choir_male: ChoirMaleIcon,
 };
